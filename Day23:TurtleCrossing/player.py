77dbfaf30b689dda64f96fc57011da55cf4c0574
Bettino Gaussaint <120162824+BettinoCodes@users.noreply.github.com>
2024-06-12 21:15:15 -0400

    Create player.py

---
+++ Day23:TurtleCrossing/player.py
@@ -0,0 +1,31 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.color('green')
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+
+    def move_up(self):
+        new_y = self.ycor() + MOVE_DISTANCE
+        self.goto(self.xcor(), new_y)
+
+    def move_down(self):
+        new_y = self.ycor() - MOVE_DISTANCE
+        self.goto(self.xcor(), new_y)
+
+    def move_left(self):
+        new_x = self.xcor() - MOVE_DISTANCE
+        self.goto(new_x, self.ycor())
+
+    def move_right(self):
+        new_x = self.xcor() + MOVE_DISTANCE
+        self.goto(new_x, self.ycor())
