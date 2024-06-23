b560ffce2a8016482690a114a56f0e40bfd7d092
Bettino Gaussaint <120162824+BettinoCodes@users.noreply.github.com>
2024-06-23 18:21:34 -0400

    Create main.py

---
+++ Day33:ISSLocationAPI/main.py
@@ -0,0 +1,63 @@
+import requests
+from datetime import datetime
+import smtplib
+import time
+
+#If the ISS is close to my current position
+# and it is currently dark
+# Then send me an email to tell me to look up.
+# BONUS: run the code every 60 seconds.
+
+MY_LAT = yours # Your latitude
+MY_LONG = yours # Your longitude
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+data = response.json()
+
+iss_latitude = float(data["iss_position"]["latitude"])
+iss_longitude = float(data["iss_position"]["longitude"])
+
+#Your position is within +5 or -5 degrees of the ISS position.
+
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+
+time_now = datetime.now()
+hour = time_now.hour
+
+
+print(iss_latitude,iss_longitude)
+
+latitude_distance = MY_LAT - iss_latitude
+longitude_distance = MY_LONG - iss_longitude
+
+my_email = "youremail@gmail.com"
+password = "yourrequest"
+
+while True:
+    time.sleep(60)
+    if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
+        if hour <= sunrise or hour >= sunset:
+            connection = smtplib.SMTP("smtp.gmail.com", port=587)
+            connection.starttls()
+
+            connection.login(user=my_email, password=password)
+            connection.sendmail(
+                from_addr=my_email,
+                to_addrs="youremail@gmail.com",
+                msg=f"Subject:ISS LOCATION ABOVE YOU\n\nLOOK UP DUDE"
+            )
+    
+            connection.close()
