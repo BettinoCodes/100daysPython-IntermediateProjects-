2c8ad61122f0cdb8fb0c60a35a270fe178166e90
Bettino Gaussaint <120162824+BettinoCodes@users.noreply.github.com>
2024-07-03 19:06:33 -0400

    Update notification_manager.py

--- Day39:FlightClubCapstone/notification_manager.py
+++ Day39:FlightClubCapstone/notification_manager.py
@@ -4,15 +4,15 @@ from twilio.rest import Client
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
     def __init__(self):
-        self.account_sid = 'id'
-        self.auth_token = 'token'
+        self.account_sid = 'yourid'
+        self.auth_token = 'yourauth'
 
-    def send_notification(self):
+    def send_notification(self, update_cheap):
         client = Client(self.account_sid, self.auth_token)
         message = client.messages.create(
-            from_='num',
-            body=f'Hello there',
-            to='yournum'
+            from_='youtilnumber',
+            body=f'{update_cheap}',
+            to='yournumbervalidated'
         )
         print(message.status)
         print(message.sid)
