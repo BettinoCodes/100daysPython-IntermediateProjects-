0487d02831c59111a3df605c0f3aead6978a97ca
Bettino Gaussaint <120162824+BettinoCodes@users.noreply.github.com>
2024-07-03 19:55:01 -0400

    Update notification_manager.py

--- Day39:FlightClubCapstone/notification_manager.py
+++ Day39:FlightClubCapstone/notification_manager.py
@@ -1,18 +1,34 @@
 import twilio
 from twilio.rest import Client
+import smtplib
 
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
     def __init__(self):
         self.account_sid = 'yourid'
-        self.auth_token = 'yourauth'
+        self.auth_token = 'yourtoken'
+        self.my_email = "youremail"
+        self.password = "yourpass"
 
     def send_notification(self, update_cheap):
         client = Client(self.account_sid, self.auth_token)
         message = client.messages.create(
-            from_='youtilnumber',
+            from_='twilionumber',
             body=f'{update_cheap}',
-            to='yournumbervalidated'
+            to='yournumber'
         )
         print(message.status)
         print(message.sid)
+
+    def send_email_notification(self, reciever, message):
+        connection = smtplib.SMTP("smtp.gmail.com", port=587)
+        connection.starttls()
+
+        connection.login(user=self.my_email, password=self.password)
+        connection.sendmail(
+            from_addr=self.my_email,
+            to_addrs=reciever,
+            msg=f"Subject:CHEAP FLIGHT DEAL\n\n{message}\nEnjoy,\n{yourname}"
+        )
+
+        connection.close()
