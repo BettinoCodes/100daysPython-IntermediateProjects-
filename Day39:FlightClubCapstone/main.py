ec4e09574ce08f559f929af5b71b3b5d12c8a69a
Bettino Gaussaint <120162824+BettinoCodes@users.noreply.github.com>
2024-07-03 19:51:54 -0400

    Update main.py

--- Day39:FlightClubCapstone/main.py
+++ Day39:FlightClubCapstone/main.py
@@ -52,6 +52,25 @@ def cheapest_by_day_each():
                             )
             notify_manage.send_notification(message_send)
 
+def cheapest_by_day_each_email():
+    list_messages = ""
+    for city_number in range(len(flight_data.data_flights)):
+        iata_code = flight_data.data_flights[city_number]["iataCode"]
+        print(f"Searching for {iata_code}...")
+        lowest_price = flight_data.data_flights[city_number]["lowestPrice"]
+        cheapest = flight_search.get_min_days(iata_code, depart_date=date_now, return_date=target_date, max_price=lowest_price)
+        if cheapest == "None":
+            list_messages += f"No flights cheaper for {iata_code} on {date_now}\n"
+        else:
+            list_messages += (
+                            f"WE FOUND CHEAPER FLIGHT TODAY FOR {iata_code}!\n"
+                            f"Price: ${cheapest["mingrand"]}\n"
+                            f"Airport: {cheapest["Flight"]}\n"
+                            f"Departure Date: {cheapest["Departure Date"]}\n"
+                            f"Return Date: {cheapest["Return Date"]}\n"
+                        )
+    return list_messages
+
 
 #adding the IATA to the sheet
 # for row_number in range(2, last_row): #last row
@@ -63,5 +82,12 @@ date_now = datetime.datetime.now()
 target_date = date_now + datetime.timedelta(days=30 * 6)
 cop_date = datetime.datetime.now()
 
+#for personal message to me
+# cheapest_by_day_each()
 
-cheapest_by_day_each()
+# for emails in data sheet
+for emails in data_manage.user_emails:
+    message = cheapest_by_day_each_email()
+    send_to = emails['whatEmailDoYouWantToUse?']
+    print(send_to)
+    notify_manage.send_email_notification(message=message, reciever=send_to)
