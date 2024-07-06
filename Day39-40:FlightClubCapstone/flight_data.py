ae9d065c44db3daa292f606f16128d68c991cc35
Bettino Gaussaint <120162824+BettinoCodes@users.noreply.github.com>
2024-07-06 18:17:30 -0400

    Update flight_data.py

--- Day39-40:FlightClubCapstone/flight_data.py
+++ Day39-40:FlightClubCapstone/flight_data.py
@@ -6,7 +6,7 @@ import json
 class FlightData:
     #This class is responsible for structuring the flight data.
     def __init__(self):
-        self.url = 'https://api.sheety.co/yourapikey/flightDealsPrices/prices'
+        self.url = 'https://api.sheety.co/00cf11bfb63c9d72833e19f6e988387d/flightDealsPrices/prices'
         self.response = requests.get(self.url)
 
         # if self.response.status_code == 200:
@@ -16,3 +16,9 @@ class FlightData:
         #     print(f"Failed to fetch data: {self.response.status_code}")
 
         self.data_flights = self.response.json()["prices"]
+        self.user_emails = self.get_users_emails()
+
+    def get_users_emails(self):
+        url = 'https://api.sheety.co/00cf11bfb63c9d72833e19f6e988387d/flightDealsPrices/users'
+        response = requests.get(url)
+        return response.json()["users"]
